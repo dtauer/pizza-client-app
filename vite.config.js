0da719f46c611992419f552225fcc7772783440c
dtauer <dustin@dtauer.com>
2024-11-19 20:53:38 -0600

    removed public reference

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://pizza-server-app.onrender.com/",
+        target: "http://localhost:3000/",
         changeOrigin: true,
       }
     },
